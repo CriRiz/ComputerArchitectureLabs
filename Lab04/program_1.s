5c0d5ecf6b98a615728777ba84c68ea3ead781ae
Cristina Rizzo <s311467@studenti.polito.it>
2025-11-01 15:19:12 +0100

    Implementato program_1.s

--- Lab04/program_1.s
+++ Lab04/program_1.s
@@ -13,27 +13,30 @@ _start:
     flw f2, 0(t2) # carica b in f2
     li t3, 16 # contatore = 16 elementi
     li t4, 0xFF # valore di confronto
-    fmv.s.x f0, x0 # accumulatore y = 0.0
+    li t5, 0x7F800000
+    fmv.s.x f0, x0 # accumulatore x = 0.0
 
 loop:
-    beqz t3, end # se contatore == 0 fine
+    beqz t3, endLoop # se contatore == 0 fine
     beq t3, t4, equal # se contatore == 0xFF vai a equal
 
     flw f1, 0(t0) # f1 = i[n]
     flw f3, 0(t1) # f3 = w[n]
     fmul.s f4, f1, f3 # f4 = i * w
-    fadd.s f4, f4, f2 # f4 = i*w + b
-    fadd.s f0, f0, f4 # y += (i*w + b)
+    fadd.s f0, f0, f4 # x += (i*w)
 
-    addi t0, t0, 4
-    addi t1, t1, 4
-    addi t3, t3, -1
+    addi t0, t0, 4  # incremento indirizzo di i
+    addi t1, t1, 4  # incremento indirizzo di w
+    addi t3, t3, -1 # decremento contatore loop
     j loop
 
-equal:
-    fmv.s.x f0, x0 # y = 0.0
-    addi t3, t3, -1
-    j loop
+endLoop:
+    fadd.s f0, f0, f2   # x += b
+    
+    # qui implemento y = f(x)
+    # 1 bit segno| 8 bit esponente| 23 bit mantissa
+
+    and t10, f0, t5 # tiro fuori l'esponente => y = f(x)
 
 end:
     li a0, 0
